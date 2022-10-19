e7da06fe74db7a5a5bb82d4be441def9b5c84f64
ysk_hello <hello.ysk1905@gmail.com>
2022-10-19 19:39:14 +0900

    https://github.com/nnn-training/node-js-http-3013
    Basic認証
    投稿一覧表示
    ログアウトのリンク
    ルーティング追加
    users.htpasswd追加

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# pug
+# npmパッケージ
+## pug
 ```
 yarn add pug@3.0.2
+```
+
+## http-auth
+```
+yarn add http-auth@4.1.9
+```
+
+# users.htpasswd
+```
+guest1:test
 ```
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict';
 const http = require('http');
+const auth = require('http-auth');
 const router = require('./lib/router');
 
-const server = http.createServer((req, res) => {
+const basic = auth.basic({
+  realm: 'Enter username and password.',
+  file: './users.htpasswd'
+});
+
+const server = http.createServer(basic.check((req, res) => {
   //console.info('create server');
   router.route(req, res);
-})
+}))
 .on('error', e => {console.error('server error', e)})
 .on('clientError', e => {console.error('client error'), e});
 
